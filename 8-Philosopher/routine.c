5839e5f1b9a85fb4ca4f4d6123a6c9dfee0936fa
Anthony Gabriel <antgabri@bess-f1r1s7.clusters.42paris.fr>
2024-02-07 14:11:01 +0100

    Philo avance

--- 8-Philosopher/routine.c
+++ 8-Philosopher/routine.c
@@ -6,7 +6,7 @@
 /*   By: Monsieur_Canard <Monsieur_Canard@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 11:58:22 by Monsieur_Ca       #+#    #+#             */
-/*   Updated: 2024/02/01 17:17:19 by Monsieur_Ca      ###   ########.fr       */
+/*   Updated: 2024/02/07 12:53:52 by Monsieur_Ca      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,8 @@ static int	eat_impair(t_list *philo)
 	pthread_mutex_lock(philo->fork_left);
 	if (verif_dead(philo) == 1)
 	{
-		pthread_mutex_unlock(philo->fork_left);
 		pthread_mutex_unlock(philo->fork_right);
+		pthread_mutex_unlock(philo->fork_left);
 		return (1);
 	}
 	(print_forks(philo), print_eating(philo));
@@ -36,8 +36,8 @@ static int	eat_impair(t_list *philo)
 	pthread_mutex_unlock(philo->is_eating_mutex);
 	if (philo_is_eating(philo) == 1)
 		return (1);
-	pthread_mutex_unlock(philo->fork_left);
 	pthread_mutex_unlock(philo->fork_right);
+	pthread_mutex_unlock(philo->fork_left);
 	return (0);
 }
 
@@ -54,8 +54,8 @@ static int	eat_pair(t_list *philo)
 	pthread_mutex_lock(philo->fork_right);
 	if (verif_dead(philo) == 1)
 	{
-		pthread_mutex_unlock(philo->fork_right);
 		pthread_mutex_unlock(philo->fork_left);
+		pthread_mutex_unlock(philo->fork_right);
 		return (1);
 	}
 	(print_forks(philo), pthread_mutex_lock(philo->is_eating_mutex));
@@ -65,8 +65,8 @@ static int	eat_pair(t_list *philo)
 	pthread_mutex_unlock(philo->is_eating_mutex);
 	if (philo_is_eating(philo) == 1)
 		return (1);
-	pthread_mutex_unlock(philo->fork_right);
 	pthread_mutex_unlock(philo->fork_left);
+	pthread_mutex_unlock(philo->fork_right);
 	return (0);
 }
 
@@ -77,8 +77,6 @@ static int	sleep_philo(t_list *philo)
 	philo->start_sleep = get_time_of_the_day();
 	philo->end_sleep = philo->start_sleep + philo->t_sleep;
 	print_sleeping(philo);
-	if (verif_dead(philo) == 1)
-		return (1);
 	usleep(philo->t_sleep * 1000);
 	if (verif_dead(philo) == 1)
 		return (1);
@@ -106,6 +104,8 @@ void	*routine(void *arg)
 	t_list	*philo;
 
 	philo = (t_list *)arg;
+	if (philo->index % 2 == 0)
+		usleep(philo->t_eat * 1000);
 	while (1)
 	{
 		if (dispatch_philo(philo) == 1)
@@ -121,8 +121,6 @@ void	*routine(void *arg)
 		if (verif_dead(philo) == 1)
 			return (0);
 		if (sleep_philo(philo) == 1)
-			return (0);
-		if (verif_dead(philo) == 1)
 			return (0);
 	}
 	return (0);
